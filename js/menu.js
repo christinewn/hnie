7d01308822c7fbb32e3e483e2cf5e0edcd08a455
christinewn <1165707819@qq.com>
2018-07-29 14:47:43 +0800

    8 commit

--- js/menu.js
+++ js/menu.js
@@ -7,7 +7,7 @@ function mouseOver(i) {
 	obj.style.left=x+"px";
 	obj.style.display = "inline";
 
-	if (i != 1&&i !=4&&i !=5 &&i !=8) {
+	if (i != 1&&i !=4&&i !=8) {
 		document.getElementById("banner_shadow").style.display="inline";
 		document.getElementById("child_menu"+i).style.display="inline";
 	}
@@ -16,7 +16,7 @@ function mouseOver(i) {
 function mouseOut(i) {
 	document.getElementById("menu_text"+i).style.color="#000000";
 	document.getElementById("menu_red_bg").style.display="none";
-	if (i != 1&&i !=4&&i !=5 &&i !=8) {
+	if (i != 1&&i !=4&&i !=8) {
 		document.getElementById('banner_shadow').style.display="none";
 		document.getElementById("child_menu"+i).style.display="none";
 	}
